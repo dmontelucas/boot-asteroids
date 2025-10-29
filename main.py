0266cfd132b2054486cce52d49e51d33ff8253b9
dmontelucas <dmonte.lucas.r@gmail.com>
2025-10-29 21:11:00 +1100

    Initial project commit. main.py, constants.py created. Black screen programmed.

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import *
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+black = (0, 0, 0)
+         
+def main():
+    global running
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+        pygame.display.flip()
+        
+
+
+                             
+            
+        
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
